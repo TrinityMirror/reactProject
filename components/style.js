32805a984e3b5349bf9f9ea21f37684162068c20
Pongpol <labinstall@tnistudent.ac.th>
2022-06-22 09:22:22 +0700

    first commit

---
+++ components/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet} from 'react-native'
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:'center',
+      alignContent:'center'
+    },
+    title:{
+      fontSize:20,
+      color:"#cc0099"
+    },
+    warning:{
+      color:"#a52a2a"
+    }
+  
+  });
+  export default styles
