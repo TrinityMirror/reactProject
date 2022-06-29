721ed2ed642ae183eedea27988883fe18f05703c
Pongpol <trinitymirrorswitch@gmail.com>
2022-06-29 10:26:40 +0700

    29 06 2022 10:26

---
+++ Cat.js
@@ -0,0 +1,20 @@
+import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <View>
+      <Text>Hello, there... I'm a catny</Text>
+      {/* <TextInput
+                    placeholder='type here'
+                    style={{ 
+                        width: '80%', 
+                        borderColor: 'gray', 
+                        borderWidth: 1, 
+                        padding: 10 
+                    }}/> */}
+    </View>
+  )
+}
+
+export default Cat
