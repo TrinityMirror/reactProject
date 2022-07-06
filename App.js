4f5543820d8c97647d6b521f0a4f0ade35148c70
Pongpol <trinitymirrorswitch@gmail.com>
2022-07-06 11:11:43 +0700

    07/06/2022

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
 import React from 'react'
 import Logo from './components/Logo'
 import User from './components/User'
-
+import LotsOfGreetings from './components/LotsOfGreetings'
+import MyCustomTextWth from './components/MyCustomTextWth'
+import Count from './components/Count'
+import InputText from './components/InputText'
+import UsernameandPassword from './components/UsernameandPassWord'
 const App = () => {
   return (
     <View>
-     <Logo/>
-     <User/>
+     {/* <Logo/> */}
+     {/* <User/> */}
+     {/* <LotsOfGreetings/> */}
+     {/* <MyCustomTextWth/> */}
+     {/* <Count/> */}
+     {/* <InputText/> */}
+     <UsernameandPassword/>
     </View>
   )
 }
 
 export default App
+
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
